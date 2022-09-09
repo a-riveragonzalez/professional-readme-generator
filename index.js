285078aa92c5352a0753cd05eefc5ea35c175be0
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2022-09-08 19:51:58 -0700

    added generate markdown function, added question array, and init function

--- index.js
+++ index.js
@@ -1,13 +1,62 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input", 
+        name: "projectTitle", 
+        message: "Type you title here",
+    }, 
+    {
+        type: "input", 
+        name: "projectDescription", 
+        message: "Provide a short description explaining the what, why, and how of your project",
+    },
+    {
+        type: "input", 
+        name: "projectInstallation", 
+        message: "What are the steps required to install your project? ",
+    }, 
+    {
+        type: "input", 
+        name: "projectUsage", 
+        message: "Provide instructions and examples for use",
+    }, 
+    {
+        type: "input", 
+        name: "projectCredits", 
+        message: "List your collaborators, if any, with links to their GitHub profiles.",
+    }, 
+    {
+        type: "list", 
+        name: "projectLicense", 
+        message: "Choose your License: ",
+        choices: ["MIT License", "Mozilla Public License 2.0", "Open Database License (ODbL)"], //add more later
+    }, 
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt (questions)
+    .then((answers)=>{
+        // console.log(answers);
+        const markdownOutput = generateMarkdown(answers);
+        console.log(markdownOutput)
+        fs.writeFile("README.md", markdownOutput, (err)=>{
+            if (err){
+                console.log("error")
+            } else {
+                console.log("README Generated")
+            }
+        })
+    })
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,69 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license==="MIT License"){
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+  }
+  if (license==="Mozilla Public License 2.0"){
+    return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+  }
+  if (license==="Open Database License (ODbL)"){
+    return "[![License: ODbL](https://img.shields.io/badge/License-ODbL-brightgreen.svg)](https://opendatacommons.org/licenses/odbl/)"
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// the actual link 
+function renderLicenseLink(license) {
+  if (license==="MIT License"){
+    return ""
+  }
+  if (license==="Mozilla Public License 2.0"){
+    return ""
+  }
+  if (license==="Open Database License (ODbL)"){
+    return "https://opendatacommons.org/licenses/odbl/"
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// short summary of license that you pick 
+function renderLicenseSection(license) {
+  if (license==="MIT License"){
+    return ""
+  }
+  if (license===""){
+    return ""
+  }
+  if (license==="Open Database License (ODbL)"){
+    return ""
+  }
+}
+
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
+function generateMarkdown(answers) {
+  return `# ${answers.projectTitle}
+${renderLicenseBadge(answers.projectLicense)}
+## Description
+${answers.projectDescription}
+## Table of Contents 
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+## Installation
+${answers.projectInstallation}
+## Usage
+${answers.projectUsage}
+## Credits
+${answers.projectCredits}
+## License
+${answers.projectLicense}
+${renderLicenseLink(answers.projectLicense)}
+${renderLicenseSection(answers.projectLicense)}
 `;
 }
 
